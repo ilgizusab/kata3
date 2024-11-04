45745da1201fb09996731598387191fbc2a69890
ILGIZ SAB <ilgizastr@gmail.com>
2024-11-04 19:48:01 +0300

    настроил соединение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kata2",
+                "root",
+                "12345678"
+        );
+        return conn;
+    }
+
 }
